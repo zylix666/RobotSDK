5f3020dc6f4afae0e34d28f3a2465c7cf8165a90
chang-ning <spiderpower02@gmail.com>
2013-09-15 17:16:27 +0800

    modify file

---
+++ gt_lib/project/ios_framework_app/L1/L1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  L1
+//
+//  Created by apple on 13/9/15.
+//  Copyright (c) 2013年 com.leaderg.app.L1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gt_lib/project/ios_framework_app/L1/L1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  L1
+//
+//  Created by apple on 13/9/15.
+//  Copyright (c) 2013年 com.leaderg.app.L1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
